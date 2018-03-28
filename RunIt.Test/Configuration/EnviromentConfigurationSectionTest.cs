63991b97897faa558b5b84c64eaddfef9b99f028
Augusto <dev.augusto@gmail.com>
2018-03-28 00:19:48 -0300

    Foram alterados novos arquivos para interação entre testes e comunicacao de app.config

---
+++ RunIt.Test/Configuration/EnviromentConfigurationSectionTest.cs
@@ -0,0 +1,23 @@
+﻿using RunIt.Infra.Configuration;
+using Xunit;
+
+namespace RunIt.Test.Configuration
+{
+    public class EnviromentConfigurationSectionTest
+    {
+        public EnviromentConfigurationSectionTest()
+        {
+        }
+
+        [Fact]
+        public void Get_WhenSettedConfFileWithTag_ShouldReturns_NotNullSection()
+        {
+            var section = EnviromentConfigurationSection.Get();
+            Assert.NotNull(section);
+            Assert.NotNull(section.Applications);
+            //Assert.NotNull(section.Credentials);
+            //Assert.Equal(1, section.Applications.Count);
+            //Assert.Equal(2, section.Credentials.Count);
+        }
+    }
+}
